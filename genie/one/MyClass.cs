f275d38f510d02bd21680cd17cb1bd0fa69576f5
Lucas Natraj <lnatraj@slb.com>
2017-05-22 17:19:37 -0700

    first

---
+++ genie/one/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.Web;
+
+namespace one
+{
+    public class MyClass
+    {
+		public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, TraceWriter log)
+		{
+			log.Info("C# HTTP trigger function processed a request. RequestUri={req.RequestUri}");
+
+			// parse query parameter
+			string name = req.GetQueryNameValuePairs()
+				.FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+				.Value;
+
+			// Get request body
+			dynamic data = await req.Content.ReadAsAsync<object>();
+
+			// Set name to query string or body data
+			name = name ?? data?.name;
+
+			return name == null
+				? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+				: req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+		}
+    }
+}
